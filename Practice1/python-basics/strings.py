fc9ced6eb272f55d5e0f8b1955a9bbab00bd360f
anort546 <a_nort@kbtu.kz>
2026-01-30 02:00:07 +0500

    strings4

--- Practice1/python-basics/strings.py
+++ Practice1/python-basics/strings.py
@@ -1,2 +1,23 @@
 a = "Hello"
 print(a)
+
+
+print("Hello")
+print('Hello')
+
+
+a = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
+print(a)
+
+
+a = "Hello, World!"
+print(a.upper())
+
+
+a = "Hello"
+b = "World"
+c = a + b
+print(c)
