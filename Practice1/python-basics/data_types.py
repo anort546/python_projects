02c5eadc5845938964993b02100e1dbff26a225f
anort546 <a_nort@kbtu.kz>
2026-01-30 01:55:15 +0500

    datatypes3

--- Practice1/python-basics/data_types.py
+++ Practice1/python-basics/data_types.py
@@ -1,5 +1,21 @@
 x = list(("apple", "banana", "cherry"))
-
 print(x)
-
 print(type(x)) 
+
+x = 5
+print(type(x))
+
+x = "Hello World"
+print(x)
+print(type(x)) 
+
+
+x = range(10)
+print(x)
+print(list(x))
+
+
+x = True
+print(x)
+print(type(x)) 
+
