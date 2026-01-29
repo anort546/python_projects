418ddfd3893df609715629c3ba9224308a458c18
anort546 <a_nort@kbtu.kz>
2026-01-30 02:02:43 +0500

    variables2

--- Practice1/python-basics/variables.py
+++ Practice1/python-basics/variables.py
@@ -2,3 +2,29 @@ x = 5
 y = "John"
 print(x)
 print(y)
+
+
+x = str(3)    
+y = int(3)    
+z = float(3)
+print(x)
+print(y)
+print(z)
+
+
+x, y, z = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)
+
+x = "Python"
+y = "is"
+z = "awesome"
+print(x, y, z)
+
+x = "Python "
+y = "is "
+z = "awesome"
+print(x + y + z)
+
+
