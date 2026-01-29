9f621e3b436580464c2748692ad3920a8c96de0d
anort546 <a_nort@kbtu.kz>
2026-01-30 01:57:58 +0500

    numbers5

--- Practice1/python-basics/numbers.py
+++ Practice1/python-basics/numbers.py
@@ -4,3 +4,29 @@ z = 1j
 print(type(x))
 print(type(y))
 print(type(z))
+
+
+x = 1
+y = 35656222554887711
+z = -3255522
+print(type(x))
+print(type(y))
+print(type(z))
+
+x = 3+5j
+y = 5j
+z = -5j
+print(type(x))
+print(type(y))
+print(type(z))
+
+import random
+print(random.randrange(1, 10))
+
+
+x = 35e3
+y = 12E4
+z = -87.7e100
+print(type(x))
+print(type(y))
+print(type(z))
