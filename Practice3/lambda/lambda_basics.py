21a23562cb90ba62dfbd483893400ad133491ab8
anort546 <a_nort@kbtu.kz>
2026-02-19 15:52:44 +0500

    commit1

---
+++ Practice3/lambda/lambda_basics.py
@@ -0,0 +1,21 @@
+#adding 5 to number (10)
+s=lambda x: x+5
+print(s(10))
+
+
+#return nuber times 7
+q=lambda x:x*7
+print(q(4))
+
+#sum of two numbers
+e=lambda x,y: x+y
+print(e(7,3))
+
+#returns square of number 
+i=lambda b: b**2
+print(i(5))
+
+#checks if number is more than 10 returns true, otherwise false
+u=lambda n: n>10
+print(u(14))
+print(u(7))
