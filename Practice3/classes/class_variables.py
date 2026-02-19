76b5ad7e84037f4c58a46679381c27239230c791
anort546 <a_nort@kbtu.kz>
2026-02-19 18:12:07 +0500

    commit4

---
+++ Practice3/classes/class_variables.py
@@ -0,0 +1,33 @@
+#prints dachshund for every dog
+class Dog:
+    species="dachshund"
+d1=Dog()
+d2=Dog()
+print(d1.species,d2.species)
+
+#prints amount of wheels
+class Car:
+    wheels = 4
+c1 = Car()
+print(c1.wheels)
+
+#prints high school for every student
+class Student:
+    school = "High School"
+s1 = Student()
+s2 = Student()
+print(s1.school, s2.school)
+
+#prtints amount of shops in circle
+class Circle:
+    shop=5
+c = Circle()
+print(c.shop)
+
+#prints man or woman
+class people:
+    man1="man"
+    woman1="woman"
+b1 = people()
+b2 = people()
+print(b1.man1, b2.woman1)
