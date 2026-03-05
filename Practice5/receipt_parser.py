05cb82a80c5ac2e627ae08a4565e68d4b0a26e2b
anort546 <a_nort@kbtu.kz>
2026-03-05 19:03:35 +0500

    commit3

--- Practice5/receipt_parser.py
+++ Practice5/receipt_parser.py
@@ -1,3 +1,49 @@
 with open("Practice5/raw.txt", "r", encoding="utf-8-sig") as f:
     text = f.read()
 
+import re
+#all prices
+patt1 = r"x\s(\d[\d\s]*,\d{2})"
+prices = re.findall(patt1, text)
+print("all prices",prices)
+
+#all product names
+patt2=r"\d+\.\n([^\d\n]+)"
+names=re.findall(patt2,text)
+print("all names",names)
+
+#total amount
+total=0
+for price in prices:
+    price_num = float(price.replace(",", ".").replace(" ", ""))
+    total += price_num
+print(f"total summ:{total:.2f}")
+
+#Extract date and time information
+patt4=r"Время:\s*(.+)\n"
+date=re.search(patt4,text)
+print("дата и время:", date.group(1))
+
+#Find payment method
+pay = r"(Банковская карта|Наличные)"
+pay1 = re.search(pay, text)
+if pay1:
+    print("метод оплаты:", pay1.group(1))
+else:
+    print("метод оплаты: Неизвестно")
+
+#Create a structured output (JSON or formatted text)
+
+print("\n" + "="*40)
+print("обзор чека")
+print("="*40)
+for name, price in zip(names, prices):
+    print(f"{name.strip()} — Цена за штуку: {price}")
+print("-"*40)
+print(f"Общая сумма: {total:.2f}")
+print(f"Дата и время: {date.group(1)}")
+if pay1:
+    print(f"Метод оплаты: {pay1.group(1)}")
+else:
+    print("Метод оплаты: Неизвестно")
+print("="*40)
